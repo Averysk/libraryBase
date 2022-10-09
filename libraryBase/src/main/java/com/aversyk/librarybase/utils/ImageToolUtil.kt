f2e5bbce845b937147c9164e5cf34df323bb9639
pengjianggui <646130507@qq.com>
2022-10-09 14:47:15 +0800

    更新 ImageToolUtil 工具类

--- libraryBase/src/main/java/com/aversyk/librarybase/utils/ImageToolUtil.kt
+++ libraryBase/src/main/java/com/aversyk/librarybase/utils/ImageToolUtil.kt
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat
  * @author Averysk
  */
 object ImageToolUtil {
+
     /**
      * 设置TextView左边图片
      * @param srcId
@@ -39,4 +40,35 @@ object ImageToolUtil {
             tv.setCompoundDrawables(null, null, db, null)
         }
     }
+
+    /**
+     * 设置TextView上面图片
+     * @param srcId
+     */
+    fun serTopIcon(context: Context?, tv: TextView, srcId: Int) {
+        if (context == null) {
+            return
+        }
+        val db = ContextCompat.getDrawable(context, srcId)
+        if (db != null) {
+            db.setBounds(0, 0, db.minimumWidth, db.minimumHeight)
+            tv.setCompoundDrawables(null, db, null, null)
+        }
+    }
+
+    /**
+     * 设置TextView下面图片
+     * @param srcId
+     */
+    fun serBottomIcon(context: Context?, tv: TextView, srcId: Int) {
+        if (context == null) {
+            return
+        }
+        val db = ContextCompat.getDrawable(context, srcId)
+        if (db != null) {
+            db.setBounds(0, 0, db.minimumWidth, db.minimumHeight)
+            tv.setCompoundDrawables(null, null, null, db)
+        }
+    }
+
 }
